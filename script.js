726fc7a82a8da8e99a53a0768d10f9e517b2f172
Ian Rose <ianrosedev@gmail.com>
2016-03-27 19:15:30 -0400

    Have search working

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-/*$(function() {
-  $.ajax({
-    url: 'http://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&meta=&continue=&titles=San+Francisco&rvprop=content&rvparse=1&rvsection=0',
-    //url: 'http://en.wikipedia.org/wiki/Special:Random&format=json',
-    //data: queryData,
-    dataType: 'jsonp',
-    //type: 'POST',
-    //headers: { 'Api-User-Agent': 'ianrose@gmail.com' },
-    success: 
-      function(data) {
-      console.log(data['query']);
-      
-    }
+ $(function() {
+  // Search Wikipedia
+  $("#searchterm").keyup(function(e) {
+    var q = $("#searchterm").val();
+    $.getJSON("http://en.wikipedia.org/w/api.php?callback=?",
+      {
+        srsearch: q,
+        action: "query",
+        list: "search",
+        format: "json"
+      },
+    function(data) {
+      $("#results").empty();
+      $("#results").append("<p>Results for <b>" + q + "</b></p>");
+      $.each(data.query.search, function(i,item){
+      $("#results").append("<div><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + "</a><br>" + item.snippet + "<br><br></div>");
+      });
+    });
   });
-  
-});*/
 
-$(function() {
   // Button redirects to random wiki page
   $('#random').click(function() {
     window.location.href = 'http://en.wikipedia.org/wiki/Special:Random';
